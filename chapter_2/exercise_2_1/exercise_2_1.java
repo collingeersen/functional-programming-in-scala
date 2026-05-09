7002594dbb2cebc07c9315f2a38ab3e4b6060ddc
Collin Geersen <collingeersen@gmail.com>
2026-05-09 01:42:08 -0400

    add: exercise 2-1 in java temp

---
+++ chapter_2/exercise_2_1/exercise_2_1.java
@@ -0,0 +1,12 @@
+class Exercise_2_1{
+    public static void main(String args[]){
+        int testValue = 6;
+
+        System.out.print(fib(testValue));
+    }
+
+    private static int fib(int n){
+        //
+    }
+
+}
